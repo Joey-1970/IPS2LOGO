fa7baef1dfc0faa77f59fb3c3ab256ee99cef124
Joey-1970 <paeper@horburg.de>
2018-08-10 07:07:34 +0200

    Update module.php

--- IPS2LOGO_Taster/module.php
+++ IPS2LOGO_Taster/module.php
@@ -16,10 +16,9 @@
             	parent::Create();
 		$this->ConnectParent("{1B0A36F7-343F-42F3-8181-0748819FB324}");
             	$this->RegisterPropertyBoolean("Open", false);
-		$this->RegisterPropertyInteger("State_ID", 0);
-		$this->RegisterPropertyInteger("Switch_ID", 0);
+		
 		$this->RegisterPropertyInteger("Switchtime", 20);
-		$this->RegisterPropertyInteger("Timer_1", 60);
+		$this->RegisterPropertyInteger("Timer_1", 250);
 		$this->RegisterTimer("Timer_1", 0, 'I2LTaster_GetState($_IPS["TARGET"]);');
 		
 		//Status-Variablen anlegen
@@ -37,11 +36,10 @@
 		$arrayElements = array(); 
 		$arrayElements[] = array("name" => "Open", "type" => "CheckBox",  "caption" => "Aktiv"); 
  		$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________"); 
-		$arrayElements[] = array("type" => "Label", "label" => "Status");
-		$arrayElements[] = array("type" => "SelectVariable", "name" => "State_ID", "caption" => "Variablen ID");
-		$arrayElements[] = array("type" => "Label", "label" => "Schalter");
-		$arrayElements[] = array("type" => "SelectVariable", "name" => "Switch_ID", "caption" => "Variablen ID");
+		
 		$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________");
+		$arrayElements[] = array("type" => "Label", "label" => "Abfragezyklus");
+		$arrayElements[] = array("type" => "IntervalBox", "name" => "Timer_1", "caption" => "ms");
 		$arrayElements[] = array("type" => "Label", "label" => "Laufzeit des Impulses");
 		$arrayElements[] = array("type" => "IntervalBox", "name" => "Switchtime", "caption" => "ms");
 
@@ -55,20 +53,14 @@
                 // Diese Zeile nicht löschen
                 parent::ApplyChanges();
 		
-		// Registrierung für die Änderung des Status
-		If ($this->ReadPropertyInteger("State_ID") > 0) {
-			$this->RegisterMessage($this->ReadPropertyInteger("Status_ID"), 10603);
-		}
-		
-            	
-		If (($this->ReadPropertyBoolean("Open") == true) AND  
-			($this->ReadPropertyInteger("State_ID") > 0) AND 
-			($this->ReadPropertyInteger("Switch_ID") > 0)) {
-			SetValueBoolean($this->GetIDForIdent("State"), GetValueBoolean($this->ReadPropertyInteger("State_ID")));
+		If ($this->ReadPropertyBoolean("Open") == true) {
+			$this->GetState();
 			$this->SetStatus(102);
+			$this->SetTimerInterval("Timer_1", $this->ReadPropertyInteger("Timer_1") );
 		}
 		else {
 			$this->SetStatus(104);
+			$this->SetTimerInterval("Timer_1", 0);
 		}
 		
 	}
@@ -77,31 +69,19 @@
 	{
   		switch($Ident) {
 	        case "State":
-	            If ($this->ReadPropertyBoolean("Open") == true) {
-		    	//$this->Set_Status($Value);
-		    }
+		    	$this->SetState($Value);
 	            break;
 	        default:
 	            throw new Exception("Invalid Ident");
 	    	}
 	}
 	    
-	public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
-    	{
-		switch ($Message) {
-			case 10803:
-				//$this->SendDebug("ReceiveData", "Ausloeser Wochenplan", 0);
-				break;
-			case 10603:
-				// Änderung der LOGO Status Variablen
-				If ($SenderID == $this->ReadPropertyInteger("State_ID")) {
-					$this->SendDebug("ReceiveData", "Ausloeser Aenderung Status", 0);
-					SetValueBoolean($this->GetIDForIdent("State"), GetValueBoolean($this->ReadPropertyInteger("State_ID")));
-				}
-				
-				break;
-		}
-    	}    
+	public function ReceiveData($JSONString) 
+	{
+	    	// Empfangene Daten vom Gateway/Splitter
+	    	$data = json_decode($JSONString);
+	 	$this->SendDebug("ReceiveData", $data, 0);
+ 	}
 	
 	// Beginn der Funktionen
 	// Simuliert einen Tastfunktion auf der Logo
@@ -111,10 +91,10 @@
 	    // Daten: {"DataID":"{042EF3A2-ECF4-404B-9FA2-42BA032F4A56}","Function":5,"Area":132,"AreaAddress":0,"BitAddress":0,"WordLength":1,"DataCount":1,"DataPayload":"\u0000"}
 	    // Ausgang abfragen
 	    // Daten: {"DataID":"{042EF3A2-ECF4-404B-9FA2-42BA032F4A56}","Function":4,"Area":130,"AreaAddress":0,"BitAddress":0,"WordLength":1,"DataCount":1,"DataPayload":""}
-	public function S7_WriteBit(Bool $State)
+	public function SetState(Bool $State)
 	{
 		If (($this->ReadPropertyBoolean("Open") == true) AND ($this->HasActiveParent() == true)) {
-			$this->SendDebug("S7_WriteBit", "Ausfuehrung", 0);
+			$this->SendDebug("SetState", "Ausfuehrung", 0);
 			If ($State = true) {
 				$DataPayload = "\u0001";
 			}
@@ -122,7 +102,20 @@
 				$DataPayload = "\u0000";
 			}
 			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{042EF3A2-ECF4-404B-9FA2-42BA032F4A56}", "Function" => 5, "Area" => 132, "AreaAddress" => 0, "BitAddress" => 0, "WordLength" => 1,"DataCount" => 1,"DataPayload" => $DataPayload)));
-			$this->SendDebug("S7_WriteBit", "Ergebnis: ".$Result, 0);
+			$this->SendDebug("SetState", "Ergebnis: ".intval($Result), 0);
+		}
+	}
+	    
+	public function GetState()
+	{
+		If (($this->ReadPropertyBoolean("Open") == true) AND ($this->HasActiveParent() == true)) {
+			$this->SendDebug("GetState", "Ausfuehrung", 0);
+			// Ausgang 1: Daten: {"DataID":"{042EF3A2-ECF4-404B-9FA2-42BA032F4A56}","Function":4,"Area":130,"AreaAddress":0,"BitAddress":0,"WordLength":1,"DataCount":1,"DataPayload":""}
+
+			$Area = 130;
+			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{042EF3A2-ECF4-404B-9FA2-42BA032F4A56}", "Function" => 4, "Area" => $Area, "AreaAddress" => 0, "BitAddress" => 0, "WordLength" => 1,"DataCount" => 1,"DataPayload" => "")));
+			$this->SendDebug("GetState", "Ergebnis: ".$Result, 0);
+			SetValueBoolean($this->GetIDForIdent("State"), $Result);
 		}
 	}
 	    
