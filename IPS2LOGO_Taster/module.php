9c9bccccf54e2f8d17357e9c72e4e2cc5278f099
Joey-1970 <paeper@horburg.de>
2018-08-11 18:15:30 +0200

    Update module.php

--- IPS2LOGO_Taster/module.php
+++ IPS2LOGO_Taster/module.php
@@ -117,7 +117,7 @@
 			}
 			else {
 				//$DataPayload = "\u0000";
-				$DataPayload = utf8_encode(chr(1));
+				$DataPayload = utf8_encode(chr(0));
 			}
 			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{042EF3A2-ECF4-404B-9FA2-42BA032F4A56}", "Function" => 5, "Area" => $Area, "AreaAddress" => $Address, "BitAddress" => $Bit, "WordLength" => 1,"DataCount" => 1,"DataPayload" => $DataPayload)));
 			$this->SendDebug("SetState", "Ergebnis: ".intval($Result), 0);
