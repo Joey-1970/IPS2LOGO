65abfea7a6f04fc1368c85d5166d4f58ab8c5caf
Joey-1970 <paeper@horburg.de>
2018-08-11 21:33:11 +0200

    Update module.php

--- IPS2LOGO_Taster/module.php
+++ IPS2LOGO_Taster/module.php
@@ -39,16 +39,21 @@
 		$arrayElements = array(); 
 		$arrayElements[] = array("name" => "Open", "type" => "CheckBox",  "caption" => "Aktiv"); 
  		$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________"); 
+		$arrayElements[] = array("type" => "Label", "label" => "Auswahl des digitalen Ausgangs oder Merkers"); 
 		$arrayOptions = array();
 		for ($i = 1; $i <= 16; $i++) {
 		    	$arrayOptions[] = array("label" => "Q".$i, "value" => $i);
 		}
+		for ($i = 1; $i <= 27; $i++) {
+		    	$arrayOptions[] = array("label" => "M".$i, "value" => $i);
+		}
 		$arrayElements[] = array("type" => "Select", "name" => "Output", "caption" => "Ausgang", "options" => $arrayOptions );
 		$arrayElements[] = array("type" => "IntervalBox", "name" => "Timer_1", "caption" => "ms");
 		$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________");
+		$arrayElements[] = array("type" => "Label", "label" => "Auswahl des Netzwerkeingangs"); 
 		$arrayElements[] = array("type" => "NumberSpinner", "name" => "Address",  "caption" => "Adresse"); 
 		$arrayElements[] = array("type" => "NumberSpinner", "name" => "Bit",  "caption" => "Bit"); 
-		$arrayElements[] = array("type" => "Label", "label" => "Laufzeit des Impulses");
+		$arrayElements[] = array("type" => "Label", "label" => "Laufzeit des Tast-Impulses");
 		$arrayElements[] = array("type" => "IntervalBox", "name" => "Switchtime", "caption" => "ms");
 
 		
@@ -92,35 +97,23 @@
  	}
 	
 	// Beginn der Funktionen
-	// Simuliert einen Tastfunktion auf der Logo
-	// Netzwerkbefehl ein
-	    // Daten: {"DataID":"{042EF3A2-ECF4-404B-9FA2-42BA032F4A56}","Function":5,"Area":132,"AreaAddress":0,"BitAddress":0,"WordLength":1,"DataCount":1,"DataPayload":"\u0001"}
-	    // aus
-	    // Daten: {"DataID":"{042EF3A2-ECF4-404B-9FA2-42BA032F4A56}","Function":5,"Area":132,"AreaAddress":0,"BitAddress":0,"WordLength":1,"DataCount":1,"DataPayload":"\u0000"}
-	    // Ausgang 1 abfragen 
-	    // Daten: {"DataID":"{042EF3A2-ECF4-404B-9FA2-42BA032F4A56}","Function":4,"Area":130,"AreaAddress":0,"BitAddress":0,"WordLength":1,"DataCount":1,"DataPayload":""}
-	// Ausgang 2
-	    //Daten: {"DataID":"{042EF3A2-ECF4-404B-9FA2-42BA032F4A56}","Function":4,"Area":130,"AreaAddress":0,"BitAddress":1,"WordLength":1,"DataCount":1,"DataPayload":""}
-	    
 	public function SetState(Bool $State)
 	{
 		If (($this->ReadPropertyBoolean("Open") == true) AND ($this->HasActiveParent() == true)) {
-			//Daten: {"DataID":"{042EF3A2-ECF4-404B-9FA2-42BA032F4A56}","Function":5,"Area":132,"AreaAddress":0,"BitAddress":0,"WordLength":1,"DataCount":1,"DataPayload":"\u0001"}
-			//Daten: {"DataID":"{042EF3A2-ECF4-404B-9FA2-42BA032F4A56}","Function":5,"Area":132,"AreaAddress":0,"BitAddress":0,"WordLength":1,"DataCount":1,"DataPayload":"\u0000"}
 			$this->SendDebug("SetState", "Ausfuehrung", 0);
-			$Area = 132; // Konstante
-			$Address = $this->ReadPropertyInteger("Address");
-			$Bit = $this->ReadPropertyInteger("Bit");
-			If ($State == true) {
-				//$DataPayload = "\u0001";
-				$DataPayload = utf8_encode(chr(1));
+			If ($State <> GetValueBoolean($this->GetIDForIdent("State"))) {
+				$Area = 132; // Konstante
+				$Address = $this->ReadPropertyInteger("Address");
+				$Bit = $this->ReadPropertyInteger("Bit");
+				If ($State == true) {
+					$DataPayload = utf8_encode(chr(1));
+				}
+				else {
+					$DataPayload = utf8_encode(chr(0));
+				}
+				$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{042EF3A2-ECF4-404B-9FA2-42BA032F4A56}", "Function" => 5, "Area" => $Area, "AreaAddress" => $Address, "BitAddress" => $Bit, "WordLength" => 1,"DataCount" => 1,"DataPayload" => $DataPayload)));
+				$this->SendDebug("SetState", "Ergebnis: ".intval($Result), 0);
 			}
-			else {
-				//$DataPayload = "\u0000";
-				$DataPayload = utf8_encode(chr(0));
-			}
-			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{042EF3A2-ECF4-404B-9FA2-42BA032F4A56}", "Function" => 5, "Area" => $Area, "AreaAddress" => $Address, "BitAddress" => $Bit, "WordLength" => 1,"DataCount" => 1,"DataPayload" => $DataPayload)));
-			$this->SendDebug("SetState", "Ergebnis: ".intval($Result), 0);
 		}
 	}
 	    
