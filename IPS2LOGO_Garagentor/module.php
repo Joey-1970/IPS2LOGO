cbaf1564c13d4078d24102f7dcb06d29ae27f913
Joey-1970 <paeper@horburg.de>
2018-08-27 20:07:41 +0200

    Update module.php

--- IPS2LOGO_Garagentor/module.php
+++ IPS2LOGO_Garagentor/module.php
@@ -149,7 +149,8 @@
 			}
 			$this->SetTimerInterval("Timer_1", $this->ReadPropertyInteger("Timer_1"));
 			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{042EF3A2-ECF4-404B-9FA2-42BA032F4A56}", "Function" => 5, "Area" => $Area, "AreaAddress" => 0, "BitAddress" => $AddressBit, "WordLength" => 1,"DataCount" => 1,"DataPayload" => $DataPayload)));
-			$this->SendDebug("SetState", "Ergebnis: ".intval($Result), 0);
+			//$this->SendDebug("SetState", "Ergebnis: ".intval($Result), 0);
+			$this->GetGateState();
 		}
 	}
 	        
