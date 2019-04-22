5224629db92a4ddfa4c3b220d6a8cf1c3affdf6e
Joey-1970 <paeper@horburg.de>
2019-04-22 11:13:32 +0200

    Update module.php

--- IPS2LOGO_Tueroeffner/module.php
+++ IPS2LOGO_Tueroeffner/module.php
@@ -131,7 +131,7 @@
 	return $Status;
 	}
 	    
-	private function HasActiveParent()
+	protected function HasActiveParent()
     	{
 		$Instance = @IPS_GetInstance($this->InstanceID);
 		if ($Instance['ConnectionID'] > 0)
