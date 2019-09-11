07021c9767b50679359c58be100f8a984652b74c
Joey-1970 <paeper@horburg.de>
2019-09-11 17:26:56 +0200

    Update module.php

--- IPS2LOGO_Rolladen/module.php
+++ IPS2LOGO_Rolladen/module.php
@@ -228,6 +228,18 @@
 			WFC_PushNotification($WebfrontID, $Title, substr($Text, 0, 256), $Sound, $TargetID);
 		}
 	}        
+	
+	private function GetWebfrontID()
+	{
+    		$guid = "{3565B1F2-8F7B-4311-A4B6-1BF1D868F39E}"; // Webfront Konfigurator
+    		//Auflisten
+    		$WebfrontArray = (IPS_GetInstanceListByModuleID($guid));
+    		$Result = array();
+    		foreach ($WebfrontArray as $Webfront) {
+        		$Result[$Webfront] = IPS_GetName($Webfront);
+    		}
+	return $Result;   
+	}    
 	    
 	private function GetParentID()
 	{
